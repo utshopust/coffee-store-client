c4eba54e77718b4d3389c1d836d5120b7c54b1d1
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-11-30 12:03:03 +0600

    Create Add coffee from Client Side using daisyui

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,11 +1,98 @@
-import React from 'react';
+import React from "react";
 
 const AddCoffee = () => {
-    return (
-        <div>
-            
+    const handleAddCoffee = e =>{
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        console.log(name,chef,supplier,taste,category,details);
+    }
+  return (
+    <div className="p-24">
+      <div className="p-12 text-center space-y-4">
+        <h1 className="text-6xl ">Add New Coffee</h1>
+        <p className="">
+          It is a long established fact that a reader will be distraceted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using Content here.
+        </p>
+      </div>
+      <form onSubmit={handleAddCoffee}>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+            <label className="label">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="input w-full"
+              placeholder="Enter Coffee Name"
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+            <label className="label">Chef</label>
+            <input
+              type="text"
+              name="chef"
+              className="input w-full"
+              placeholder="Enter coffee chef "
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+            <label className="label">Supplier</label>
+            <input
+              type="text"
+              name="supplier"
+              className="input w-full"
+              placeholder="Enter Coffee Supplier"
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+            <label className="label">Taste</label>
+            <input
+              type="text"
+              name="taste"
+              className="input w-full"
+              placeholder="Enter Coffee Taste"
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+            <label className="label">Category</label>
+            <input
+              type="text"
+              name="category"
+              className="input w-full"
+              placeholder="Enter Coffee Category"
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+            <label className="label">Details</label>
+            <input
+              type="text"
+              name="details"
+              className="input w-full"
+              placeholder="Enter Coffee Details"
+            />
+          </fieldset>
         </div>
-    );
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border my-6 p-4">
+          <label className="label">Photo</label>
+          <input
+            type="text"
+            name="photo"
+            className="input w-full"
+            placeholder="Enter Photo URL"
+          />
+        </fieldset>
+        <input type="submit" value="Add Coffee"  className="btn w-full bg-yellow-500"/>
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
