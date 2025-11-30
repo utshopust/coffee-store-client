0f9f986f0894bbe6195cd1119e4eedb8315faeff
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-11-30 11:14:57 +0600

    structure the mainlayout

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,29 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import MainLayout from './layouts/MainLayout.jsx';
+import Home from './components/Home.jsx';
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: 'addCoffee',
+        Component: AddCoffee
+      },
+      {
+        path: 'updateCoffee',
+        Component: UpdateCoffee
+      }
+    ]
   },
 ]);
 
