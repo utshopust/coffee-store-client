e4fab5fe2b0fbab01bc2e29fb98bb6cb3bd776d6
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-12-01 21:21:05 +0600

    create a user for the database

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,32 +1,88 @@
 import React, { use } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
   const { createUser } = use(AuthContext);
   console.log(createUser);
 
-  const handleSignUp = e =>{
-    e.preventDefault()
+  const handleSignUp = (e) => {
+    e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email,password);
+    console.log(email, password);
+
+    const name = e.target.name.value;
+    const address = e.target.address.value;
+    const phone = e.target.phone.value;
+    const photo = e.target.photo.value;
+    const userProfile = { name, address, phone, photo };
+    console.log(userProfile);
     // Create User in the Firebase
 
-    createUser(email,password)
-    .then(result=>{
+    createUser(email, password)
+      .then((result) => {
         console.log(result.user);
-    })
-     .catch((error) => {
-    // const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage);
-  });
-  }
+        // save profile info into DB
+        fetch("http://localhost:3000/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(userProfile),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.insertedId) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Your Account Is Created",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          });
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
   return (
     <div className="card bg-base-100 mx-auto max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className="text-5xl font-bold">Sign Up now!</h1>
         <form onSubmit={handleSignUp} className="fieldset">
+          <label className="label">Name</label>
+          <input
+            type="text"
+            name="name"
+            className="input"
+            placeholder="Your Name"
+          />
+          <label className="label">Address</label>
+          <input
+            type="text"
+            name="address"
+            className="input"
+            placeholder="Your Address"
+          />
+          <label className="label">Phone</label>
+          <input
+            type="text"
+            name="phone"
+            className="input"
+            placeholder="Your Phone"
+          />
+          <label className="label">Photo</label>
+          <input
+            type="text"
+            name="photo"
+            className="input"
+            placeholder="Photo URL"
+          />
           <label className="label">Email</label>
           <input
             type="email"
