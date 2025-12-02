982d368af4d55bbbe4e3c878a14f256dcd6f1fa2
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-12-02 15:07:21 +0600

    Delete user and  signin user

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const SignIn = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email,password);
+  };
+  return (
+    <div className="card bg-base-100 mx-auto max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-5xl font-bold">Sign In now!</h1>
+        <form onSubmit={handleSignIn} className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Sign In</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,12 +1,44 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const initialUsers = useLoaderData();
   const [users, setUsers] = useState(initialUsers);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // server side specific user
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              const remainingUsers = users.filter((user) => user._id !== id);
+              setUsers(remainingUsers);
+              Swal.fire({
+                title: "Deleted!",
+                text: "User has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div>
-      <h2 className="text-3xl text-center">Total: {initialUsers.length}</h2>
+      <h2 className="text-3xl text-center">Total: {users.length}</h2>
 
       <div className="overflow-x-auto">
         <table className="table">
@@ -22,11 +54,9 @@ const Users = () => {
           </thead>
           <tbody>
             {/* row 1 */}
-            {users.map((user,index) => (
+            {users.map((user, index) => (
               <tr key={user._id}>
-                <th>
-                    {index+1}
-                </th>
+                <th>{index + 1}</th>
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
@@ -43,13 +73,18 @@ const Users = () => {
                     </div>
                   </div>
                 </td>
-                <td>
-                  
-                  {user.phone}
-                  
-                </td>
+                <td>{user.phone}</td>
                 <td>{user.email}</td>
-                
+                <th>
+                  <button className="btn btn-xs">V</button>
+                  <button className="btn btn-xs">E</button>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-xs"
+                  >
+                    X
+                  </button>
+                </th>
               </tr>
             ))}
           </tbody>
